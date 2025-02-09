4eaedbfb949fb2ad5b97e50cf148580ff71b4bea
tishkavlone <damn.sogood@yandex.ru>
2025-02-09 08:15:26 +0300

    Realization API_ACESS for BingX Exchange

--- bot/services/client/client_side.py
+++ bot/services/client/client_side.py
@@ -1,5 +1,7 @@
 import asyncio
+import traceback
 from bot.services.api.bybit import BybitClient, BybitAPIException
+from bot.services.api.bingx import BingXClient, BingXAPIException
 
 async def get_bybit():
     async with BybitClient() as client:
@@ -10,4 +12,15 @@ async def get_bybit():
         except BybitAPIException as e:
             print(f"Error: {e}")
        
+       
+async def get_bingx():
+    async with BingXClient() as client:
+        try:
+            trading_pairs = await client.get_trading_pairs()
+            order_books = await client.get_all_orderbooks(trading_pairs)
+            return order_books
+    # TODO: Время выполнения (+-100сек, ограничено api)
+        except BingXAPIException as e:
+            print(f"Erorr: {e}")
+
       
